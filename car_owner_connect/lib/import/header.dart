702f909a05dd07b1e4c908bfad25f380a7dbff82
Nkoyama <n.koyama24@gmailcom>
2020-09-21 21:28:00 +0900

    title bar 色変更

--- car_owner_connect/lib/import/header.dart
+++ car_owner_connect/lib/import/header.dart
@@ -7,7 +7,10 @@ class Header extends StatelessWidget with PreferredSizeWidget{
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text('sign in'),
+      title: Text('sign in',),
+      backgroundColor: Colors.lightGreen,
+      centerTitle: true,
+      elevation: 0.0,
     );
   }
 }
