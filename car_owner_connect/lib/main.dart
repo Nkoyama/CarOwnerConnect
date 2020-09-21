a92170f34a509b8d11ddf2463fb12ee52095c0bb
Nkoyama <n.koyama24@gmailcom>
2020-09-21 22:27:52 +0900

    username & password

--- car_owner_connect/lib/main.dart
+++ car_owner_connect/lib/main.dart
@@ -12,7 +12,66 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: Header(),
-        body: Center(child: Text("オラオラオラオラ")),
+        body: MyApp(),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _State();
+  }
+}
+
+class _State extends State<MyApp> {
+  String _text = 'Enter something...';
+
+  void _userNameSubmitted(String value) {
+    setState(() {
+      _text = 'Submitted $value';
+    });
+  }
+
+  void _passwordSubmitted(String value) {
+    setState(() {
+      _text = 'Submitted $value';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Username',
+                hintText: 'Username',
+                icon: Icon(Icons.account_circle),
+              ),
+              autocorrect: false,
+              autofocus: true,
+              keyboardType: TextInputType.text,
+              onSubmitted: _userNameSubmitted,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                hintText: 'Password',
+                icon: Icon(Icons.security),
+              ),
+              autocorrect: false,
+              autofocus: false,
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              onSubmitted: _passwordSubmitted,
+            ),
+          ],
+        ),
       ),
     );
   }
