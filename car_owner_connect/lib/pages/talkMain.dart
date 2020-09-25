8be20015e80d02ebb6ab7afee1a5c0bf35df608d
Nkoyama <n.koyama24@gmailcom>
2020-09-25 19:39:59 +0900

    検索項目いくつか設置

--- car_owner_connect/lib/pages/talkMain.dart
+++ car_owner_connect/lib/pages/talkMain.dart
@@ -8,7 +8,7 @@ class TalkMain extends StatelessWidget {
       appBar: Header(headerText: 'talk',),
       body: Container(
         child: TalkMainPage(),
-        color: Colors.white,
+        color: Colors.lightBlue,
       ),
     );
   }
@@ -21,32 +21,88 @@ class TalkMainPage extends StatelessWidget {
       body: Column(
         children: [
 
-          // search
+          /* search */
           Container(
             constraints: BoxConstraints.expand(height: 25.0),
-            padding: EdgeInsets.all(0),
+            padding: EdgeInsets.only(left:5, right:5),
             child: Text(
-                '検索',
-                style: TextStyle(
-                    color: Colors.black,
-                  fontSize: 18,
-                )
+              '検索',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+              )
             ),
             color: Colors.grey
           ),
 
-          // history
-          Container(
-              constraints: BoxConstraints.expand(height: 25.0),
-              padding: EdgeInsets.all(0),
-              child: Text(
-                  '履歴',
+          Row(
+            children: [
+              Expanded(child: Container(
+                padding: EdgeInsets.only(left:10.0, right:10.0, top:3.0, bottom:1.5),
+                child: Text("本拠"),
+                height: 25,
+                color: Colors.lightBlue,
+              ),),
+              Expanded(child: Container(
+                padding: EdgeInsets.only(left:10.0, right:10.0, top:3.0, bottom:1.5),
+                child: TextField(
                   style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
+                    fontSize: 15.0
                   )
-              ),
-              color: Colors.grey
+                ),
+                height: 25.0,
+              ),),
+              Expanded(child: Container(
+                padding: EdgeInsets.only(left:10.0, right:10.0, top:3.0, bottom:1.5),
+                child: Text("分類番号"),
+                height: 25,
+                color: Colors.lightBlue,
+              ),),
+              Expanded(child: Container(
+                padding: EdgeInsets.only(left:10.0, right:10.0, top:3.0, bottom:1.5),
+                child: TextField(
+                  style: TextStyle(
+                    fontSize: 15.0
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                height: 25.0,
+              ),),
+            ],
+          ),
+
+          Row(
+            children: [
+              Expanded(child: Container(
+                padding: EdgeInsets.only(left:10.0, right:10.0, top:3.0, bottom:1.5),
+                child: Text("色"),
+                height: 25,
+                color: Colors.lightBlue,
+              ),),
+              Expanded(child: Container(
+                padding: EdgeInsets.only(left:10.0, right:10.0, top:3.0, bottom:1.5),
+                child: TextField(
+                    style: TextStyle(
+                        fontSize: 15.0
+                    )
+                ),
+                height: 25.0,
+              ),),
+            ],
+          ),
+
+          /* history */
+          Container(
+            constraints: BoxConstraints.expand(height: 25.0),
+            padding: EdgeInsets.only(left:5, right:5),
+            child: Text(
+              '履歴',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+              )
+            ),
+            color: Colors.grey
           ),
 
           SingleChildScrollView(
