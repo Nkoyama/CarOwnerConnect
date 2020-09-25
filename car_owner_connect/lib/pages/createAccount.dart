173dcc37523c632ea577c6de2a95b02bad9e67c7
Nkoyama <n.koyama24@gmailcom>
2020-09-25 16:31:55 +0900

    アカウント作成画面 新しいusername入力欄

--- car_owner_connect/lib/pages/createAccount.dart
+++ car_owner_connect/lib/pages/createAccount.dart
@@ -7,7 +7,45 @@ class CreateAccount extends StatelessWidget {
     return Scaffold(
       appBar: Header(headerText: 'create account',),
       body: Container(
-        height: double.infinity,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // logo
+              Container(
+                  padding: EdgeInsets.all(15),
+                  child: Image.asset('images/CaroCon_logo2.png')
+              ),
+
+              // new username textField
+              Container(
+                padding: EdgeInsets.only(right:20, left:20),
+                child: Column(
+                  children: <Widget>[
+
+                    // username textField
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'new username',
+                        hintText: 'mew username',
+                        icon: Icon(Icons.account_circle),
+                      ),
+                      autocorrect: false,
+                      autofocus: false,
+                      keyboardType: TextInputType.text,
+                    ),
+
+                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                      Text(
+                        '　　　※半角',
+                        textAlign: TextAlign.left,
+                      ),
+                    ]),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
         color: Colors.white,
       ),
     );
