1325518aa316ff61dde7368362c7fae8dcb5fdc2
Nkoyama <n.koyama24@gmailcom>
2020-10-11 22:52:09 +0900

    公式Twitterリンク作成中

--- car_owner_connect/lib/pages/forgetPassword.dart
+++ car_owner_connect/lib/pages/forgetPassword.dart
@@ -1,22 +1,65 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../import/header.dart';
 
 class ForgetPassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Header(headerText: 'forget password',),
-      body: Container(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-
-              ),
-            ],
-          )
+        appBar: Header(headerText: 'forget password',),
+        body: Container(
+            child: new SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                          "以下の秘密の質問に答えてください。"
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "万が一、秘密の質問の答えが分からない場合は、公式Twitterにてご相談ください。",
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: OpenTwitter(),
+                    ),
+                  ],
+                )
+            )
         )
+    );
+  }
+}
+
+class OpenTwitter extends StatefulWidget {
+  OpenTwitter({Key key}) : super(key: key);
+
+  @override
+  OpenTwitterState createState() => new OpenTwitterState();
+}
+
+class OpenTwitterState extends State<OpenTwitter> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: InkWell(
+        child: Text("公式Twitter"),
+        onTap: launchTwitter,
       )
     );
+  }
+
+  void launchTwitter() async {
+    const url = 'https://www.google.com/';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
