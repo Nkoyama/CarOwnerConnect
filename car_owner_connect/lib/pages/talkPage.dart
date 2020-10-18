520c829a4905c212da1c3b5066290161851978b2
Nkoyama <n.koyama24@gmailcom>
2020-10-18 17:47:42 +0900

    トークページのテキスト入力部分作成’

--- car_owner_connect/lib/pages/talkPage.dart
+++ car_owner_connect/lib/pages/talkPage.dart
@@ -13,7 +13,37 @@ class TalkPage extends StatelessWidget {
       appBar: Header(headerText: opponentText),
       body: Container(
 
-      )
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () => {},
+            ),
+            IconButton(
+              icon: Icon(Icons.camera_alt),
+              onPressed: () => {},
+            ),
+            IconButton(
+              icon: Icon(Icons.photo),
+              onPressed: () => {},
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '',
+                ),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.mic),
+              onPressed: () => {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
