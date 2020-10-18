4d6eadb709b3e78f87867bdf667c82d6cac76d66
Nkoyama <n.koyama24@gmailcom>
2020-10-18 17:30:31 +0900

    トークページ

---
+++ car_owner_connect/lib/pages/talkPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '../import/header.dart';
+
+class talkPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: Header(headerText: 'talk'),
+      body: Container(
+
+      )
+    );
+  }
+}
